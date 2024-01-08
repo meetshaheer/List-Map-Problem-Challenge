95ac3a7cedc184dfe3425e7910ff8c4f9dca47ac
meetshaheer <shaheer.ahmed0404@gmail.com>
2024-01-08 19:23:40 +0500

    List Map Indexing, Retrieving Value Challenge

---
+++ List-Map-Challenge.dart
@@ -0,0 +1,46 @@
+void main(List<String> args) {
+  // List Map Challenge
+  // Get the value "bye" from 3rd Map
+  List haha = [
+    {
+      0: 0,
+      1: [1],
+    },
+    {"2": "2"},
+    {
+      1: {
+        "hehe": [
+          {
+            "hehe": {"haha": "bye"}
+          }
+        ],
+      }
+    }
+  ];
+
+  // dont see below line before your own logic. First solve using your own thinking.
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+
+  print(haha[2][1]["hehe"][0]["hehe"]["haha"]);
+}
